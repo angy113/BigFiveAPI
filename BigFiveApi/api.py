c62b3cc258c0cbaba6e415d2df03d3373a090ed1
Angela Valdez Bernal <Angela.Valdez@jalasoft.com>
2021-08-16 16:55:08 -0400

    Add prediction model

--- BigFiveApi/api.py
+++ BigFiveApi/api.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 from .serializers import SurveySerializer
 from .models import Survey
 
@@ -11,34 +12,34 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.preprocessing import LabelEncoder
 
+
 class SurveyViewSet(ModelViewSet):
     queryset = Survey.objects.all()
     serializer_class = SurveySerializer
 
-    @action(detail=False, methods=['post'])
-    def predict (self, request, pk = None):
-        
-        res = json.loads(request.data)
-        array1 = np.fromiter(res.values(),dtype=int)
-        arrayAnswers = array1[0:50] / 50
+    def create(self, request, *args, **kwargs):
 
-        reduceTimes = lambda t: 60000 if t>60000 else t
+        res = json.loads(request.data)
+        array1 = np.fromiter(res.values(), dtype=int)
+        arrayAnswers = array1[0:50] / 5
+
+        def reduceTimes(t): return 60000 if t > 60000 else t
         vfunc = np.vectorize(reduceTimes)
-        arrayTimes=vfunc(array1[50:100])
+        arrayTimes = vfunc(array1[50:100])
         arrayTimes = arrayTimes / 60000
-        
-        array1 = np.append(arrayAnswers,arrayTimes)
+
+        array1 = np.append(arrayAnswers, arrayTimes)
         array = np.array([array1])
         model = keras.models.load_model('/home/ann_model.h5')
         prediction = model.predict(array)
 
-        # Prediccion 
+        # Prediccion
         pred = []
 
-        for i in prediction:    
+        for i in prediction:
             pred.append(np.where(i == np.amax(i))[0][0])
 
-        # Load label encoder 
+        # Load label encoder
         encoder = LabelEncoder()
         encoder.classes_ = np.load('/home/classes.npy', allow_pickle=True)
-        return Response(encoder.inverse_transform([pred])[0])
+        return Response(encoder.inverse_transform([pred])[0], status=HTTP_201_CREATED)
