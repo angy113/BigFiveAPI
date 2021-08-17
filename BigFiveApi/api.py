7033a1cc879355f2e7e5b908962f6adc8f171b6f
Angela Valdez Bernal <Angela.Valdez@jalasoft.com>
2021-08-16 20:02:24 -0400

    set files paths

--- BigFiveApi/api.py
+++ BigFiveApi/api.py
@@ -8,7 +8,6 @@ from .models import Survey
 import numpy as np
 
 from tensorflow import keras
-from tensorflow.keras import layers
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -17,17 +16,18 @@ class SurveyViewSet(ModelViewSet):
     serializer_class = SurveySerializer
 
     def create(self, request, *args, **kwargs):
-        array1 = np.fromiter(request.data.values(), dtype=int)
-        arrayAnswers = array1[0:50] / 5
+        super().create(request,*args, **kwargs)
+        requestNumphyArray = np.fromiter(request.data.values(), dtype=int)
+        arrayAnswers = requestNumphyArray[0:50] / 5
 
         def reduceTimes(t): return 60000 if t > 60000 else t
         vfunc = np.vectorize(reduceTimes)
-        arrayTimes = vfunc(array1[50:100])
+        arrayTimes = vfunc(requestNumphyArray[50:100])
         arrayTimes = arrayTimes / 60000
 
-        array1 = np.append(arrayAnswers, arrayTimes)
-        array = np.array([array1])
-        model = keras.models.load_model('/home/ann_model.h5')
+        requestNumphyArray = np.append(arrayAnswers, arrayTimes)
+        array = np.array([requestNumphyArray])
+        model = keras.models.load_model('./ann_model.h5')
         prediction = model.predict(array)
 
         # Prediccion
@@ -38,5 +38,5 @@ class SurveyViewSet(ModelViewSet):
 
         # Load label encoder
         encoder = LabelEncoder()
-        encoder.classes_ = np.load('/home/classes.npy', allow_pickle=True)
+        encoder.classes_ = np.load('./classes.npy', allow_pickle=True)
         return Response(encoder.inverse_transform([pred])[0], status=HTTP_201_CREATED)
